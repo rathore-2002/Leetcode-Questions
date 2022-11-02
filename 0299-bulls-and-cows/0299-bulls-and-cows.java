91c75e31e08046b2ebe404c9959d285afec7fce9
Priya Rathore <92203739+rathore-2002@users.noreply.github.com>
2022-11-02 19:32:45 +0530

    Time: 7 ms (85.19%), Space: 42.7 MB (64.63%) - LeetHub

---
+++ 0299-bulls-and-cows/0299-bulls-and-cows.java
@@ -0,0 +1,27 @@
+class Solution {
+    public String getHint(String secret, String guess) {
+        int bulls = 0, cows = 0;
+
+        Map<Character, Integer> secretFreq = new HashMap<>();
+        Map<Character, Integer> guessFreq = new HashMap<>();
+
+        for (int i = 0; i < secret.length(); i++) {
+            char s = secret.charAt(i);
+            char g = guess.charAt(i);
+
+            if (s == g) bulls++;
+            else {
+                secretFreq.put(s, secretFreq.getOrDefault(s, 0) + 1);
+                guessFreq.put(g, guessFreq.getOrDefault(g, 0) + 1);
+            }
+        }
+
+        for (char c : secretFreq.keySet()) {
+            if (guessFreq.containsKey(c)) {
+                cows += Math.min(secretFreq.get(c), guessFreq.get(c));
+            }
+        }
+
+        return bulls + "A" + cows + "B";
+    }
+}
