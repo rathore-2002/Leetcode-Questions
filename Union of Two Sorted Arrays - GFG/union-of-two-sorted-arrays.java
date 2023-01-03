c05ad0f2508386fabf1bd8d7a1de3ecceefaf0c1
Priya Rathore <92203739+rathore-2002@users.noreply.github.com>
2023-01-03 18:55:07 +0530

    Added solution - LeetHub

--- Union of Two Sorted Arrays - GFG/union-of-two-sorted-arrays.java
+++ Union of Two Sorted Arrays - GFG/union-of-two-sorted-arrays.java
@@ -55,34 +55,37 @@ class Solution
     public static ArrayList<Integer> findUnion(int arr1[], int arr2[], int n, int m)
     {
         // add your code here
-        int i = 0, j = 0; // pointers
-  ArrayList<Integer > Union=new ArrayList<>(); // Uninon vector
-  while (i < n && j < m) {
-    if (arr1[i] <= arr2[j]) // Case 1 and 2
-    {
-      if (Union.size() == 0 || Union.get(Union.size()-1) != arr1[i])
-        Union.add(arr1[i]);
-      i++;
-    } else // case 3
-    {
-      if (Union.size() == 0 || Union.get(Union.size()-1) != arr2[j])
-        Union.add(arr2[j]);
-      j++;
-    }
-  }
-  while (i < n) // IF any element left in arr1
-  {
-    if (Union.get(Union.size()-1) != arr1[i])
-      Union.add(arr1[i]);
-    i++;
-  }
-  while (j < m) // If any elements left in arr2
-  {
-    if (Union.get(Union.size()-1) != arr2[j])
-      Union.add(arr2[j]);
-    j++;
-  }
-  return Union;
+        ArrayList<Integer> ans = new ArrayList<>();
+        int i=0,j=0;
+        while(i<arr1.length && j<arr2.length){
+            if(arr1[i]<=arr2[j]){
+                if(ans.size()==0 || ans.get(ans.size()-1)!=arr1[i])
+                ans.add(arr1[i]);
+                i++;
+                
+            }
+            else{
+                if(ans.size()==0 || ans.get(ans.size()-1)!=arr2[j])
+                ans.add(arr2[j]);
+                j++;
+                
+            }
+            
+        }
+        
+        while(i<arr1.length){
+            if(ans.get(ans.size()-1)!=arr1[i])
+            ans.add(arr1[i]);
+            i++;
+            
+        }
+        while(j<arr2.length){
+            if(ans.get(ans.size()-1)!=arr2[j])
+            ans.add(arr2[j]);
+            j++;
+            
+        }
+        return ans;
     }
 }
 
