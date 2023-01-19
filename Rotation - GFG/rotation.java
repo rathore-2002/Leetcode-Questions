290d447526c8558221f66ce9698f2ff1b6265658
Priya Rathore <92203739+rathore-2002@users.noreply.github.com>
2023-01-20 01:57:32 +0530

    Added solution - LeetHub

---
+++ Rotation - GFG/rotation.java
@@ -0,0 +1,41 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    // Driver code
+    public static void main(String[] args) throws Exception {
+        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine().trim());
+        while (t-- > 0) {
+            int n = Integer.parseInt(br.readLine().trim());
+            String[] str = br.readLine().trim().split(" ");
+            int[] a = new int[n];
+            for (int i = 0; i < n; i++) {
+                a[i] = Integer.parseInt(str[i]);
+            }
+
+            int ans = new Solution().findKRotation(a, n);
+            System.out.println(ans);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    int findKRotation(int arr[], int n) {
+        // code here
+        for(int i=0;i<n-1;i++){
+            if(arr[i]>arr[i+1])
+            return i+1;
+        }
+        return 0;
+    }
+}
