665124afda96535b14ee25b3ecf068ad87c89914
Priya Rathore <92203739+rathore-2002@users.noreply.github.com>
2023-02-02 00:54:56 +0530

    Time: 1 ms (99.91%), Space: 45.7 MB (91.08%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.java
@@ -0,0 +1,16 @@
+class Solution {
+   // Moore voting algorithm
+
+    public int majorityElement(int[] nums) {
+    int count=0, ret = 0;
+    for (int num: nums) {
+        if (count==0)
+            ret = num;
+        if (num!=ret)
+            count--;
+        else
+            count++;
+    }
+    return ret;
+   }
+}
