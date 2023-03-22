d100dd4cc29e534654809cd055fa183e6e77ab53
Priya Rathore <92203739+rathore-2002@users.noreply.github.com>
2023-03-22 16:36:12 +0530

    Added solution - LeetHub

--- Replace elements by its rank in the array - GFG/replace-elements-by-its-rank-in-the-array.java
+++ Replace elements by its rank in the array - GFG/replace-elements-by-its-rank-in-the-array.java
@@ -29,46 +29,29 @@ class GFG {
 
 
 //User function Template for Java
+
+
+//User function Template for Java
+
 class Solution {
-
-    static int[] replaceWithRank(int arr[], int n) {
-
-    // code here
-
-        int brr[] = arr.clone();
-
-        Arrays.sort(brr);
-
-        
-
-        HashMap<Integer, Integer>hm = new HashMap<>();
-
-        //(element, rank)
-
-        int rank = 1;
-
-        for(int i=0; i<n; i++){
-
-            if(hm.get(brr[i]) == null){
-
-                hm.put(brr[i], rank++);
-
-            }
-
-        }
-
-        int ans[] = new int[n];
-
-        for(int i=0; i<n; i++){
-
-            ans[i] = hm.get(arr[i]);
-
-        }
-
-        return ans;
-
-        
-
-    }
-
+    static int[] replaceWithRank(int arr[], int N) {
+     // code here
+     int ans[] = new int[arr.length];
+     PriorityQueue<Integer> pq = new PriorityQueue<>();
+     for(int i : arr){
+         pq.add(i);
+     }
+     HashMap<Integer,Integer> map = new HashMap<>();
+     int ctr=1;
+     while(!pq.isEmpty()){
+         if(map.containsKey(pq.peek()))              pq.remove();
+         else
+           map.put(pq.remove(),ctr++);
+     }
+      for(int i=0;i<arr.length;i++){
+          ans[i]=map.get(arr[i]);
+      }
+      return ans;
+  }
 }
+     
