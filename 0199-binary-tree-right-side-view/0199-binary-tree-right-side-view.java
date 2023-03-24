fc150cecc00f890789d12e114213c95875ac001c
Priya Rathore <92203739+rathore-2002@users.noreply.github.com>
2023-03-25 01:05:23 +0530

    Time: 0 ms (100.00%), Space: 41.1 MB (60.83%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
@@ -14,17 +14,15 @@
  * }
  */
 class Solution {
-    public void helper(TreeNode root,List<Integer> ans,int level){
-        if(root==null)
-            return;
-        if(level==ans.size())
-            ans.add(root.val);
-        helper(root.right,ans,level+1);
-        helper(root.left,ans,level+1);
-    }
     public List<Integer> rightSideView(TreeNode root) {
         List<Integer> ans = new ArrayList<>();
         helper(root,ans,0);
         return ans;
+    }
+    private void helper(TreeNode root,List<Integer> ans,int level){
+        if(root==null)        return;
+        if(level==ans.size())        ans.add(root.val);
+        helper(root.right,ans,level+1);
+        helper(root.left,ans,level+1);
     }
 }
