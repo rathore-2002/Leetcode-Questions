e7d7288a38e7ad75d94da156374d5240642d1d23
Priya Rathore <92203739+rathore-2002@users.noreply.github.com>
2023-04-02 17:49:43 +0530

    Added solution - LeetHub

---
+++ Common elements - GFG/common-elements.java
@@ -0,0 +1,81 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+
+public class GFG
+{
+    public static void main(String args[]) 
+    {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- > 0) 
+        {
+            int n1 = sc.nextInt();
+            int n2 = sc.nextInt();
+            int n3 = sc.nextInt();
+            
+            int A[] = new int[n1];
+            int B[] = new int[n2];
+            int C[] = new int[n3];
+            
+            for (int i = 0;i < n1;i++)
+            {
+                A[i] = sc.nextInt();
+            }
+            for (int i = 0;i < n2;i++)
+            {
+                B[i] = sc.nextInt();
+            }
+            for (int i = 0;i < n3;i++)
+            {
+                C[i] = sc.nextInt();
+            }
+            
+            Solution sol = new Solution();
+            ArrayList<Integer> res = sol.commonElements(A, B, C, n1, n2, n3);
+            if (res.size() == 0)
+            {
+                System.out.print(-1);
+            }
+            else 
+            {
+                for (int i = 0;i < res.size();i++)
+                {
+                    System.out.print(res.get(i) + " ");
+                }    
+            }
+            System.out.println();
+        }
+    }
+}
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    ArrayList<Integer> commonElements(int A[], int B[], int C[], int n1, int n2, int n3) 
+    {
+        // code here 
+        int i=0,j=0,k=0,max=Integer.MIN_VALUE;
+        ArrayList<Integer> ans = new ArrayList<>();
+        while(i<n1 && j<n2 && k<n3){
+            if(A[i]==B[j] && A[i]==C[k]){
+                if(ans.size()==0 || ans.get(ans.size()-1)!=A[i])
+                ans.add(A[i]);
+                i++;j++;k++;
+            }  else{  
+            max=Math.max(A[i],Math.max(B[j],C[k]));
+            if(max!=A[i])  i++;
+            if(max!=B[j])  j++;
+            if(max!=C[k])  k++;
+            }
+            
+        }
+        return ans;
+    }
+}
