066ff7a75d0d4376de89e78b5ecb64a9cfe29c8b
Priya Rathore <92203739+rathore-2002@users.noreply.github.com>
2023-04-23 01:25:28 +0530

    Time: 4 ms (54.17%), Space: 42.7 MB (48.66%) - LeetHub

--- 0572-subtree-of-another-tree/0572-subtree-of-another-tree.java
+++ 0572-subtree-of-another-tree/0572-subtree-of-another-tree.java
@@ -14,25 +14,22 @@
  * }
  */
 class Solution {
-    public boolean isIdentical(TreeNode root,TreeNode subroot){
-        if(root==null && subroot==null)
-            return true;
-        if(root==null || subroot==null)
-            return false;
-        if(root.val==subroot.val){
-            return isIdentical(root.left,subroot.left) &&                          isIdentical(root.right,subroot.right);
-        }
-        return false;
-    }
-    public boolean isSubtree(TreeNode root, TreeNode subroot) {
-        if(subroot==null)
-            return true;
-        if(root==null)
-            return false;
-        if(root.val==subroot.val){
-            if(isIdentical(root,subroot))
+    public boolean isSubtree(TreeNode root, TreeNode subRoot) {
+        if(subRoot==null)     return true;
+        if(root==null)        return false;
+        if(root.val==subRoot.val){
+            if(isIdentical(root,subRoot))
                 return true;
         }
-        return isSubtree(root.left,subroot)|| isSubtree(root.right,subroot);
+                
+        return isSubtree(root.left,subRoot) || isSubtree(root.right,subRoot);
+    }
+    private boolean isIdentical(TreeNode root,TreeNode subRoot){
+        if(root==null && subRoot==null)      return true;
+        if(root==null || subRoot==null)       return false;
+        if(root.val!=subRoot.val)             return false;
+        return isIdentical(root.left,subRoot.left) && isIdentical(root.right,subRoot.right);
     }
 }
+
+
