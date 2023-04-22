1504b1823fc5f5d8ee7abe68029e1a2af8a0eb4b
Priya Rathore <92203739+rathore-2002@users.noreply.github.com>
2023-04-23 00:09:57 +0530

    Time: 2 ms (20.43%), Space: 39.9 MB (53.96%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int getSum(int n){
+        int s=0;
+        while(n>0){
+            int d=n%10;
+            s+=d*d;
+            n/=10;
+        }
+        return s;
+    }
+    public boolean isHappy(int n) {
+        HashMap<Integer,Integer> map = new HashMap<>();
+        while(n!=1){
+            n=getSum(n);
+            
+            if(map.containsKey(n))      return false;
+            else   map.put(n,map.getOrDefault(n,0)+1);
+        }
+        if(n==1)       return true;
+        return false;
+    }
+}
+
+
+/*HashSet<Integer> set = new HashSet<>();
+        while(n!=1){
+            n=sum(n);
+        if(set.contains(n))
+            return false;
+        else
+            set.add(n);
+            
+        }
+         
+            return true;*/
+        
