5606d341e68d8eb8ab6134b8e3cb65ee3dd8ff00
Priya Rathore <92203739+rathore-2002@users.noreply.github.com>
2023-04-23 22:38:35 +0530

    Time: 422 ms (50.75%), Space: 0B (100.00%) - LeetHub

---
+++ 1661-average-time-of-process-per-machine/1661-average-time-of-process-per-machine.sql
@@ -0,0 +1,5 @@
+# Write your MySQL query statement below
+select machine_id, 
+round(sum(case when activity_type = 'start' then -1*timestamp else timestamp end)/count(distinct process_id),3) as processing_time
+from Activity
+group by machine_id
