27702204bd25fd63d045f3cc0cc4a3c8cf40b80c
Priya Rathore <92203739+rathore-2002@users.noreply.github.com>
2023-04-27 10:13:49 +0530

    Added solution - LeetHub

---
+++ Detect cycle in a directed graph - GFG/detect-cycle-in-a-directed-graph.java
@@ -0,0 +1,59 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+class DriverClass {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+
+        while (t-- > 0) {
+            ArrayList<ArrayList<Integer>> list = new ArrayList<>();
+            int V = sc.nextInt();
+            int E = sc.nextInt();
+            for (int i = 0; i < V; i++)
+                list.add(i, new ArrayList<Integer>());
+            for (int i = 0; i < E; i++) {
+                int u = sc.nextInt();
+                int v = sc.nextInt();
+                list.get(u).add(v);
+            }
+            if (new Solution().isCyclic(V, list) == true)
+                System.out.println("1");
+            else
+                System.out.println("0");
+        }
+    }
+}
+// } Driver Code Ends
+
+
+/*Complete the function below*/
+
+class Solution {
+    // Function to detect cycle in a directed graph.
+    private boolean dfs(int node,ArrayList<ArrayList<Integer>> adj,int vis[],int pathvis[]){
+        vis[node]=1;
+        pathvis[node]=1;
+        for(int i : adj.get(node)){
+            if(vis[i]==0){
+            if(dfs(i,adj,vis,pathvis)==true)        return true;
+            }
+            else if(pathvis[i]==1)              return true;
+        }
+        pathvis[node]=0;
+        return false;
+    }
+    public boolean isCyclic(int V, ArrayList<ArrayList<Integer>> adj) {
+        // code here
+        int vis[] = new int[V];
+        int pathvis[] = new int[V];
+        for(int i=0;i<V;i++){
+            if(vis[i]==0){
+                if(dfs(i,adj,vis,pathvis)==true)     return true;
+            }
+        }
+        return false;
+    }
+}
