bc919b703f59c3041293a818b95b7e38922e710f
Priya Rathore <92203739+rathore-2002@users.noreply.github.com>
2023-10-01 21:35:26 +0530

    Time: 52 ms (21.16%), Space: 53.8 MB (92.64%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.java
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.java
@@ -4,10 +4,9 @@ class Solution {
         HashMap<Integer,Integer> map = new HashMap<>();
         for(;e<n;e++){
             map.put(fruits[e],map.getOrDefault(fruits[e],0)+1);
-            for(;map.size()>2 && l<e;l++){
-                if(map.get(fruits[l])==1)          map.remove(fruits[l]);
-                else
-                    map.put(fruits[l],map.get(fruits[l])-1);
+            for(;map.size()>2;l++){
+                if(map.get(fruits[l])==1)         map.remove(fruits[l]);
+                 else                             map.put(fruits[l],map.get(fruits[l])-1);
             }
             ans=Math.max(ans,e-l+1);
         }
