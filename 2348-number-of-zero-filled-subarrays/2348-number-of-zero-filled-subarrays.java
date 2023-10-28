cf65eb271479279eb5739962da267ed552d0a547
Priya Rathore <92203739+rathore-2002@users.noreply.github.com>
2023-10-28 23:43:55 +0530

    Time: 4 ms (20.44%), Space: 56.8 MB (32.20%) - LeetHub

--- 2348-number-of-zero-filled-subarrays/2348-number-of-zero-filled-subarrays.java
+++ 2348-number-of-zero-filled-subarrays/2348-number-of-zero-filled-subarrays.java
@@ -1,14 +1,34 @@
 class Solution {
     public long zeroFilledSubarray(int[] nums) {
-        long res = 0;
-        long length = 0;
-        for(int num : nums){
-            if(num == 0){
-                res += ++length;
-            } else {
-                length = 0;
+        int l=0,e=0,n=nums.length;
+        long sum=0,ans=0;
+        for(;e<n;e++){
+            sum+=nums[e];
+            for(;sum!=0;l++){
+                sum-=nums[l];
             }
+            ans+=(e-l+1);
         }
-        return res;
+        return ans;
     }
 }
+
+
+/*class Solution {
+    public long zeroFilledSubarray(int[] nums) {
+        int s=0;
+        int e=0;
+        long ans=0;
+        int sum=0;
+        while(e<nums.length){
+            sum+=nums[e];
+            while(sum!=0){
+                sum-=nums[s];
+                s++;
+            }
+        ans+=e-s+1;
+        e++;
+        }
+        return ans;
+    }
+}*/
