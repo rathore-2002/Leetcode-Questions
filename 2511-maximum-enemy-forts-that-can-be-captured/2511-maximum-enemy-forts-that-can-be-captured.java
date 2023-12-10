c1759e7a7f72e32b47cc9cace59496252d0ac49d
Priya Rathore <92203739+rathore-2002@users.noreply.github.com>
2023-12-10 20:19:18 +0530

    Time: 0 ms (100.00%), Space: 40.1 MB (66.04%) - LeetHub

--- 2511-maximum-enemy-forts-that-can-be-captured/2511-maximum-enemy-forts-that-can-be-captured.java
+++ 2511-maximum-enemy-forts-that-can-be-captured/2511-maximum-enemy-forts-that-can-be-captured.java
@@ -1,16 +1,11 @@
 class Solution {
-  public int captureForts(int[] forts) {
-    final int n = forts.length;
-    int result = 0;
-    for (int i = 0, j = 0; i < n; i++) {
-      if (forts[i] != 0) {
-        if (forts[i] == -forts[j]) {
-          result = Math.max(result, i - j - 1);
-        }
-        j = i;
-      }
+   public int captureForts(int[] f) {
+        int r=0,j=0;
+        for(int i=0;i<f.length;i++){
+            if(f[i]!=0){
+                if(f[j]==-f[i]) r=Math.max(r,i-j-1);
+                j=i;
+            }
+        }return r;
     }
-
-    return result;
-  }
 }
