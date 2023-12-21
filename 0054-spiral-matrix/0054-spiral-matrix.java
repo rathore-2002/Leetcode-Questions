2c7a7fe4dd9d8fba7b871caf854513e69b87e8b1
Priya Rathore <92203739+rathore-2002@users.noreply.github.com>
2023-12-21 16:59:24 +0530

    Time: 0 ms (100.00%), Space: 41.2 MB (7.83%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.java
+++ 0054-spiral-matrix/0054-spiral-matrix.java
@@ -1,28 +1,44 @@
 class Solution {
-    public List<Integer> spiralOrder(int[][] matrix) {
-        ArrayList<Integer> arr = new ArrayList<>();
-        int m=matrix.length;
-        int n=matrix[0].length;
-        int rowno=0;
-        int colno=0;
-        while(rowno<m && colno<n){
-            for(int i=colno;i<n;i++)
-                arr.add(matrix[rowno][i]);
-            rowno++;
-            for(int i=rowno;i<m;i++)
-                arr.add(matrix[i][n-1]);
-            n--;
-            if(rowno<m){
-            for(int i=n-1;i>=colno;i--)
-                arr.add(matrix[m-1][i]);
-            m--;
+    public List<Integer> spiralOrder(int[][] mat) {
+        List<Integer> ans = new ArrayList<>();
+        
+        int m = mat.length; // no. of rows
+        int n = mat[0].length; // no. of columns
+        
+        // Initialize the pointers required for traversal.
+        int top = 0, left = 0, bottom = m - 1, right = n - 1;
+
+        // Loop until all elements are not traversed.
+        while (top <= bottom && left <= right) {
+
+            // For moving left to right
+            for (int i = left; i <= right; i++)
+                ans.add(mat[top][i]);
+
+            top++;
+
+            // For moving top to bottom.
+            for (int i = top; i <= bottom; i++)
+                ans.add(mat[i][right]);
+
+            right--;
+
+            // For moving right to left.
+            if (top <= bottom) {
+                for (int i = right; i >= left; i--)
+                    ans.add(mat[bottom][i]);
+
+                bottom--;
             }
-            if(colno<n){
-                for(int i=m-1;i>=rowno;i--)
-                    arr.add(matrix[i][colno]);
-                    colno++;
+
+            // For moving bottom to top.
+            if (left <= right) {
+                for (int i = bottom; i >= top; i--)
+                    ans.add(mat[i][left]);
+
+                left++;
             }
         }
-        return arr;
+        return ans;
     }
 }
