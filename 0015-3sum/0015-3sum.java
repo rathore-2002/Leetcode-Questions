644a54609c7ca47ccc97dec20adad1b4b9fa1910
Priya Rathore <92203739+rathore-2002@users.noreply.github.com>
2023-12-21 18:26:55 +0530

    Time: 30 ms (76.33%), Space: 51.3 MB (28.42%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,31 +1,26 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> res = new ArrayList<List<Integer>>();
+        List<List<Integer>> ans = new ArrayList<>();
+        int n=nums.length;
         Arrays.sort(nums);
-        for(int i=0;i<nums.length-2;i++){
-            if(i==0 || (i>0 && nums[i]!=nums[i-1])){
-                int s=i+1;
-                int e=nums.length-1;
-                int sum=0-nums[i];
-                while(s<e){
-                    if(nums[s]+nums[e]==sum){
-                        List<Integer> ans = new ArrayList<>();
-                        ans.add(nums[i]);
-                        ans.add(nums[s]);
-                        ans.add(nums[e]);
-                        res.add(ans);
-                        
-                        while(s<e && nums[s]==nums[s+1]) s++;
-                        while(s<e && nums[e]==nums[e-1]) e--;
-                        
-                        s++;
-                        e--;
-                    }
-                    else if(nums[s]+nums[e]<sum)    s++;
-                    else                            e--;
+        for(int i=0;i<n;i++){
+            if(i!=0 && nums[i]==nums[i-1])  continue;
+            int j=i+1,k=n-1;
+            while(j<k){
+                int sum=nums[i]+nums[j]+nums[k];
+                if(sum<0)    j++;
+                else if(sum>0)    k--;
+                else{
+                    List<Integer> temp =Arrays.asList(nums[i],nums[j],nums[k]);
+                    ans.add(temp);
+                    j++;
+                    k--;
+                    
+                    while(j<k && nums[j]==nums[j-1])   j++;
+                    while(j<k && nums[k]==nums[k+1])   k--;
                 }
             }
         }
-        return res;
+        return ans;
     }
 }
