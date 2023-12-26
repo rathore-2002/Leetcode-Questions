384565f4a1314a181e2e417a19a0686b2403e132
Priya Rathore <92203739+rathore-2002@users.noreply.github.com>
2023-12-27 00:50:33 +0530

    Time: 11 ms (57.12%), Space: 44.2 MB (9.25%) - LeetHub

--- 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.java
+++ 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.java
@@ -1,24 +1,30 @@
 class Solution {
-   private int helper(int[][] dp, int n, int k, int target) {
-	
-	if (target <= 0)
-		return 0;
-	
-	if (n == 1) 
-		return (k >= target) ? 1 : 0;
-	int res = 0;
-	if (dp[n][target] != -1)
-		return dp[n][target];
-	for (int i = 1; i <= k; ++i) 
-		res = (int) ((res + helper(dp, n - 1, k, target - i)) % (1000000007));
-	return dp[n][target] = res;
-}
+    public int numRollsToTarget(int n, int k, int target) {
+        int[][] dp = new int[n+1][1000];
+        for(int i =0;i<n+1;i++)
+        {
+            Arrays.fill(dp[i],-1);
+        }
+       return helper(n,k,target,dp);
+    }
 
-public int numRollsToTarget(int n, int k, int target) {
-	
-	int[][] dp = new int[n+1][target+1];
-	for (int[] row : dp) 
-		Arrays.fill(row, -1);
-	return helper(dp, n, k, target);
-}
+    public static int helper(int n , int k , int target, int[][] dp)
+    {
+        if(n==0 && target==0)
+            return 1;
+        if(target <= 0)
+            return 0;
+        if(n<=0)
+            return 0;    
+        if(dp[n][target]!=-1)
+            return dp[n][target];
+        int ans=0;
+        for(int i =1; i<=k; i++)
+        {
+            ans+=(helper(n-1, k , target-i,dp)%1000000007);
+            ans=ans%1000000007;
+        }
+        dp[n][target]= ans;
+        return dp[n][target];
+    }
 }
