a21ec6fec58414b32e8cb44eebbf6078da88fb62
Priya Rathore <92203739+rathore-2002@users.noreply.github.com>
2024-01-03 23:53:57 +0530

    Time: 380 ms (19.14%), Space: 45.8 MB (13.15%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.java
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.java
@@ -1,51 +1,28 @@
 class Solution {
     public String frequencySort(String s) {
-        HashMap<Character, Integer> hm = new HashMap<>();
-        
-        // adding all chars to a hashmap
-        for(int i=0;i<s.length();i++)
-        {
-            hm.put(s.charAt(i), hm.getOrDefault(s.charAt(i), 0)+1);
+        HashMap<Character,Integer> map = new HashMap<>();
+        for(int i=0;i<s.length();i++){
+            map.put(s.charAt(i),map.getOrDefault(s.charAt(i),0)+1);
         }
-        
-        // creating a Pair array class and storing the char and its count
-        Pair[] arr = new Pair[hm.size()];
+        Pair[] arr = new Pair[map.size()];
         int k=0;
-         for (Map.Entry<Character, Integer> entry : hm.entrySet())
-         {
-             arr[k] = new Pair(entry.getKey(), entry.getValue());
-             k++;
-         }
-        
-        // sorting this array based on the count of each character
-        Arrays.sort(arr, (a, b) -> b.count - a.count);
-        
-        StringBuilder sb = new StringBuilder("");
-        
-        
-        // from sorted array appending all chars to a string
-        for(int i=0;i<arr.length;i++)
-        {
+        for(Character c : map.keySet()){
+            arr[k++] = new Pair(c,map.get(c));
+        }
+        Arrays.sort(arr,(a,b)->b.count-a.count);
+        String ans="";
+        for(int i=0;i<arr.length;i++){
             for(int j=0;j<arr[i].count;j++)
-            {
-                sb.append(arr[i].c);
-            }
+                ans+=arr[i].c;
         }
-        
-        return sb.toString();
-            
+        return ans;
     }
-    
 }
-
-class Pair
-{
-        Character c;
-        Integer count;
-        
-        public Pair(Character c , Integer count)
-        {
-            this.c = c;
-            this.count = count;
-        }
+class Pair{
+    char c;
+    int count;
+    public Pair(char c,int count){
+        this.c=c;
+        this.count=count;
+    }
 }
