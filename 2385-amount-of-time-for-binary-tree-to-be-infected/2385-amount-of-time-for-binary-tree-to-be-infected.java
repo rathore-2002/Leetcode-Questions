dbf90174f2790ec96364eddb4d75cc5184932c5d
Priya Rathore <92203739+rathore-2002@users.noreply.github.com>
2024-01-10 20:00:38 +0530

    Time: 8 ms (97.91%), Space: 78.4 MB (57.70%) - LeetHub

---
+++ 2385-amount-of-time-for-binary-tree-to-be-infected/2385-amount-of-time-for-binary-tree-to-be-infected.java
@@ -0,0 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+  private int ans;
+
+  public int amountOfTime(TreeNode root, int start) {
+    dfs(root, start);
+    return ans;
+  }
+
+  public int dfs(TreeNode root, int start) {
+    if (root == null) return 0;
+
+    int leftDepth = dfs(root.left, start);
+    int rightDepth = dfs(root.right, start);
+
+    if (root.val == start) {
+      ans = Math.max(leftDepth, rightDepth);
+      return -1;
+    } else if (leftDepth >= 0 && rightDepth >= 0) {
+      return Math.max(leftDepth, rightDepth) + 1;
+    } else {
+      ans = Math.max(ans, Math.abs(leftDepth - rightDepth));
+      return Math.min(leftDepth, rightDepth) - 1;
+    }
+  }
+}
