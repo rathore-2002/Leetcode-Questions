20bdcbfb9e168a7eb69dabd59482953e79ecaa73
Priya Rathore <92203739+rathore-2002@users.noreply.github.com>
2024-01-15 15:50:06 +0530

    Time: 4 ms (61.96%), Space: 44.2 MB (22.66%) - LeetHub

---
+++ 0437-path-sum-iii/0437-path-sum-iii.java
@@ -0,0 +1,56 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+   Map<Long, Integer> hmap;
+    int count;
+
+    public int pathSum(TreeNode root, int targetSum) {
+        hmap = new HashMap<>();
+        count = 0;
+
+        dfs(root, 0, targetSum);
+
+        return count;
+    }
+
+    private void dfs(TreeNode root, long prefixSum, int targetSum) {
+        // base case
+        if (root == null) return;
+
+        
+        prefixSum += root.val;
+        
+        // If map contains a sum equal to (prefixSum - targetSum), we need to increment count that many times
+        if (hmap.containsKey(prefixSum-targetSum)) {
+            count += hmap.get(prefixSum-targetSum);
+        } 
+        
+        // There can be cases when the prefixSum is directly equal to targetSum, we need to increment count
+        if (targetSum == prefixSum) {
+            count++;
+        }
+
+        // Update the prefixSum till current node and it's count
+        hmap.put(prefixSum, hmap.getOrDefault(prefixSum, 0) + 1);
+
+        // Recurse
+        dfs(root.left, prefixSum, targetSum);
+        dfs(root.right, prefixSum, targetSum);
+
+        // Backtrack
+        hmap.put(prefixSum, hmap.get(prefixSum) - 1);
+    }
+}
