bd814d6c40049fda938b875b3385f392348fda84
Priya Rathore <92203739+rathore-2002@users.noreply.github.com>
2024-01-21 00:45:57 +0530

    Time: 9 ms (61.01%), Space: 46.4 MB (37.01%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -5,10 +5,9 @@ class Solution {
             return res.toArray(new int[0][]);
         Arrays.sort(intervals,(a,b)->a[0]-b[0]);
         int s=intervals[0][0];
-        int e=intervals[0][1];
-        for(int[] i : intervals){
-            if(i[0]<=e)
-                e=Math.max(e,i[1]);
+        int e=intervals[0][0];
+        for(int i[] : intervals){
+            if(i[0]<=e)     e=Math.max(e,i[1]);
             else{
                 res.add(new int[]{s,e});
                 s=i[0];
@@ -18,4 +17,4 @@ class Solution {
         res.add(new int[]{s,e});
         return res.toArray(new int[0][]);
     }
-}                                                                             
+}
