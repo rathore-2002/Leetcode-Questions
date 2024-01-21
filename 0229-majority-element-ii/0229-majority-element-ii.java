37faac3620160159797e7077bdb2d29b0c5e01a7
Priya Rathore <92203739+rathore-2002@users.noreply.github.com>
2024-01-22 00:29:09 +0530

    Time: 2 ms (99.12%), Space: 48 MB (24.88%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -1,10 +1,10 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
+        List<Integer> ans = new ArrayList<>();
         int num1=-1,num2=-1,c1=0,c2=0;
-        ArrayList<Integer> ans = new ArrayList<>();
         for(int i : nums){
-            if(i==num1)      c1++;
-            else if(i==num2)    c2++;
+            if(i==num1)  c1++;
+            else if(i==num2)   c2++;
             else if(c1==0){
                 num1=i;
                 c1=1;
@@ -13,18 +13,15 @@ class Solution {
                 num2=i;
                 c2=1;
             }
-            else{
-                c1--;
-                c2--;
-            }
+            else  { c1--;c2--;}            
         }
         int ctr1=0,ctr2=0;
         for(int i : nums){
-            if(i==num1)    ctr1++;
-            if(i==num2)    ctr2++;
+            if(i==num1) ctr1++;
+            if(i==num2)  ctr2++;
         }
-        if(ctr1>nums.length/3)       ans.add(num1);
-        if(ctr2>nums.length/3 && num1!=num2)       ans.add(num2);
+            if(ctr1>nums.length/3)    ans.add(num1);
+            if(num1!=num2 && ctr2>nums.length/3)     ans.add(num2);
         
         return ans;
     }
