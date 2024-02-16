78110f2d6f8b0f124ebb7f99f74620455429c17a
Priya Rathore <92203739+rathore-2002@users.noreply.github.com>
2024-02-16 22:57:41 +0530

    Time: 1 ms (100.00%), Space: 45.9 MB (70.63%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -1,43 +1,26 @@
 class Solution {
-    public double findMedianSortedArrays(int[] a, int[] b) {
-        if(a.length>b.length){
-            int[] temp = a;
-            a = b;
-            b = temp;
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+         if(nums1.length>nums2.length){
+            int[] temp = nums1;
+            nums1 = nums2;
+            nums2 = temp;
         }
-        int low=0,high = a.length, te = a.length+b.length;
-        while(low<=high){
-            int aleft = low+(high-low)/2;
-            int bleft = (te+1)/2-aleft;
-
-            int alm1 = (aleft==0) ? Integer.MIN_VALUE:a[aleft-1];
-            int al = (aleft==a.length) ? Integer.MAX_VALUE:a[aleft];
-            int blm1 = (bleft==0) ? Integer.MIN_VALUE:b[bleft-1];
-            int bl = (bleft==b.length) ? Integer.MAX_VALUE:b[bleft];
-
-            //valid segregation
-            if(alm1 <= bl && blm1 <=al){
-                double median = 0.0;
-                if(te%2==0){
-                    int lmax = Math.max(alm1,blm1);
-                    int rmin = Math.min(al,bl);
-                    median = (lmax+rmin)/2.0;
-                }else{
-                    int lmax = Math.max(alm1,blm1);
-                    median = lmax;
-                }
-                  return median;
-                }
-                else if(alm1 > bl){
-                //there are more elements to be picked in left part of 'b' array. 
-                high = aleft-1;
-                }
-                else if(blm1 > al){
-                //there are more element to be picked in left part of 'a' array.
-                low = aleft+1;
-                }
-            }
+        int n1=nums1.length,n2=nums2.length;
+        int l=0,h=n1;
+        while(l<=h){
+        int cut1=l+(h-l)/2;
+        int cut2=(n1+n2)/2-cut1;
+        int l1=cut1==0?Integer.MIN_VALUE : nums1[cut1-1];
+        int l2=cut2==0?Integer.MIN_VALUE : nums2[cut2-1];
+        int r1=cut1==n1?Integer.MAX_VALUE : nums1[cut1];
+        int r2=cut2==n2?Integer.MAX_VALUE : nums2[cut2];
+        
+        if(l1>r2)          h=cut1-1;
+        else if(l2>r1)     l=cut1+1;
+        else{
+            return (n1+n2)%2==0?(Math.max(l1,l2)+Math.min(r1,r2))/2.0: Math.min(r1,r2);  
+        }
+        }
         return 0;
-
-        }
     }
+}
